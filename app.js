42a365c712cc6769bb800457b4d886df9fddc3be
unknown <ashikdas652.com>
2021-10-22 01:54:14 +0600

    commit

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ const API_KEY = `2fa971f6e036e68fd4a1d22f52a1e824
 
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q= ${city}&appid=${API_KEY}`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q= ${city}&appid=${API_KEY}&units=metric`
     fetch(url)
     .then(res => res.json())
     .then(data => displayTemperature(data));
 
 };
 
+const setInnerText = (id , text) => {
+    document.getElementById(id).innerText = text;
+}
+
+
 const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
     console.log(temperature)
 } 
