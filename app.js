c3ac62e5e6bb544840bbab2f4461f44bc1112027
unknown <ashikdas652.com>
2021-10-22 02:30:00 +0600

    commit

--- app.js
+++ app.js
@@ -18,5 +18,11 @@ const setInnerText = (id , text) => {
 const displayTemperature = temperature => {
     setInnerText('city', temperature.name);
     setInnerText('temperature', temperature.main.temp);
-    console.log(temperature)
-} 
+    setInnerText('tem-condition', temperature.weather[0].main);
+    
+    // set weather icon 
+
+    const url = ` http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`
+    const imgIcon = document.getElementById('weather-icon')
+    imgIcon.setAttribute('src', url);
+}
